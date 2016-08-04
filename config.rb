9b4956f1dcbcfbc24b7d33ee5495033d94d1771e
Security2431 <Artem2431@gmail.com>
2016-08-04 20:44:19 +0300

    init multylending

--- config.rb
+++ config.rb
@@ -3,10 +3,10 @@ require 'compass/import-once/activate'
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "css"
+css_dir = "stylesheet"
 sass_dir = "sass"
-images_dir = "img"
-javascripts_dir = "js"
+images_dir = "image"
+javascripts_dir = "javascript"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
